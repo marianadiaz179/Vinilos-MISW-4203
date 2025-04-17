faca4f449a05ba40d92dbd173ca04c9b15b0ed08
Diego Acosta <d.acostao@uniandes.edu.co>
2025-04-16 21:35:03 -0500

    ViewModel de Performer

--- app/src/main/java/com/app/vinilos_misw4203/viewmodels/PerformerViewModel.kt
+++ app/src/main/java/com/app/vinilos_misw4203/viewmodels/PerformerViewModel.kt
@@ -1,4 +1,62 @@
 package com.app.vinilos_misw4203.viewmodels
 
-class PerformerViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.app.vinilos_misw4203.models.Performer
+
+class PerformerViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _performers = MutableLiveData<List<Performer>>()
+    val performers: LiveData<List<Performer>>
+        get() = _performers
+
+    private val _eventNetworkError = MutableLiveData(false)
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    private val _isNetworkErrorShown = MutableLiveData(false)
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    init {
+        refreshDataFromNetwork()
+    }
+
+    private fun refreshDataFromNetwork() {
+        AlbumViewModel.NetworkServiceAdapter
+            .getInstance(getApplication())
+            .getPerformers(
+                {
+                    val it = null
+                    _performers.postValue(it)
+                    _eventNetworkError.value = false
+                    _isNetworkErrorShown.value = false
+                },
+                {
+                    _eventNetworkError.value = true
+                }
+            )
+    }
+
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    class Factory(private val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(PerformerViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return PerformerViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct PerformerViewModel")
+        }
+    }
+}
+
+private fun Any.getPerformers(any: Any, function: () -> Unit) {
+
 }
