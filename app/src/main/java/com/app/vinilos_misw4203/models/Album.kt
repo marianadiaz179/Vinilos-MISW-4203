5c977f979dbfd6ba9991ea99688ea51bfac80697
Diego Acosta <d.acostao@uniandes.edu.co>
2025-04-16 20:56:45 -0500

    Modelos atributos de modelos album y performer.

--- app/src/main/java/com/app/vinilos_misw4203/models/Album.kt
+++ app/src/main/java/com/app/vinilos_misw4203/models/Album.kt
@@ -1,3 +1,11 @@
 package com.app.vinilos_misw4203.models
 
-data class Album()
+data class Album(
+    val albumId: Long = 0,
+    val name: String,
+    val coverUrl: String,
+    val releaseDate: String,
+    val description: String,
+    val genre: String,
+    val recordLabel: String
+)
--- app/src/main/java/com/app/vinilos_misw4203/models/Performer.kt
+++ app/src/main/java/com/app/vinilos_misw4203/models/Performer.kt
@@ -1,3 +1,11 @@
 package com.app.vinilos_misw4203.models
 
-data class Performer()
+data class Performer(
+    val performerId: String,
+    val name: String,
+    val image: String,
+    val description: String,
+    val performerType: String,
+    val birthDate: String? = null,
+    val creationDate: String? = null
+)
