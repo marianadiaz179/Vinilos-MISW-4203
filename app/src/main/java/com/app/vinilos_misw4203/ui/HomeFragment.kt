08ffd257e8293f842c664ae2d11d4279ed6b1402
marianadiaz179 <m.diaza2@uniandes.edu.co>
2025-04-24 20:00:52 -0500

    Add navigation between home view and album list view

--- app/src/main/java/com/app/vinilos_misw4203/ui/HomeFragment.kt
+++ app/src/main/java/com/app/vinilos_misw4203/ui/HomeFragment.kt
@@ -6,13 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.app.vinilos_misw4203.R
+import androidx.navigation.fragment.findNavController
 import com.app.vinilos_misw4203.databinding.HomeFragmentBinding
-import com.app.vinilos_misw4203.ui.adapters.HomeAdapter
+import com.app.vinilos_misw4203.R
 
 class HomeFragment : Fragment() {
     private var _binding: HomeFragmentBinding? = null
@@ -24,7 +20,23 @@ class HomeFragment : Fragment() {
     ): View? {
         _binding = HomeFragmentBinding.inflate(inflater, container, false)
         val view = binding.root
-        // You can set up the UI here without needing a ViewModel
+
+        // Manejo de clics en las tarjetas
+        binding.albumContainer.setOnClickListener {
+            // Navegar al fragmento de album
+            findNavController().navigate(R.id.action_homeFragment_to_albumFragment)
+        }
+
+        binding.artistContainer.setOnClickListener {
+            // Mostrar un Toast diciendo que la funcionalidad no está disponible
+            Toast.makeText(requireContext(), "Esta funcionalidad aún no está lista", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.collectionContainer.setOnClickListener {
+            // Mostrar un Toast diciendo que la funcionalidad no está disponible
+            Toast.makeText(requireContext(), "Esta funcionalidad aún no está lista", Toast.LENGTH_SHORT).show()
+        }
+
         return view
     }
 
